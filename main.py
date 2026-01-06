28cd41794ab7eb3986bdb00e160711ad88206fa6
Katia Punter <katiapunter@gmail.com>
2026-01-06 12:15:02 +0000

    adds the quiz

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
-print("Hello world")
-print('lalalala')
+def my_function(user_answer):
+    if user_answer == 4:
+        print("That is correct!!!")
+    else:
+        print("That is INcorrect!!!")
+
+print("Welcome to out quiz")
+
+terminal_answer = int(input("What is 2 plus 2"))
+
+my_function(terminal_answer)
